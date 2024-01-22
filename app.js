185be7a3807869fac934d44de67549657877173d
Eloi Jahan <108059693+eloiJhn@users.noreply.github.com>
2024-01-22 16:26:56 +0100

    feat: :zap: Step 7 (Add Cookies and Memory data)

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+
 
 // Définition du moteur de template
 app.set('view engine', 'ejs');
@@ -22,8 +27,27 @@ app.get('/form', (req, res) => {
 app.get('/student/:id', (req, res) => {
     const { id } = req.params;
     const name = req.query.name || 'Unknown';
+
+    // Enregistrement des données dans les cookies
+    res.cookie('name', name);
+    res.cookie('number', id);
+
     res.render('student', { id, name });
 });
+
+app.get('/memory', (req, res) => {
+    const name = req.cookies.name;
+    const number = req.cookies.number;
+
+    if (name && number) {
+        res.send(`${name}, student number ${number} was here.`);
+    } else if (number) {
+        res.send(`Student number ${number} was here.`);
+    } else {
+        res.send('No data available.');
+    }
+});
+
 
 // Gestion des erreurs 404
 app.use((req, res) => {
